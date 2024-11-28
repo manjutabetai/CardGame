72482b52595e959bdb157cc979fba3926518191c
kaishi <zerokara.imasara@gmail.com>
2024-11-28 14:30:20 +0900

    BGMのオンオフにアイコンを対応

--- src/components/Ui.jsx
+++ src/components/Ui.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { NB_ROUNDS, useGameEngine } from '../hooks/useGameEngine'
 import { isHost, isStreamScreen, myPlayer } from 'playroomkit';
+import { useControls } from 'leva';
 
 const audios = {
   background: new Audio("/audios/Drunken Sailor - Cooper Cannell.mp3"),
@@ -77,10 +78,11 @@ export const  Ui = () => {
 
     // Audio Manager
     const [audioEnabled,setAudioEnabled] = useState(false)
+
     const toggleAudio = () =>{
     setAudioEnabled((prev)=>!prev);
     }
-
+    // bgm再生
   useEffect(()=>{
     if(audioEnabled){
       audios.background.play();
@@ -96,7 +98,7 @@ export const  Ui = () => {
 
   },[audioEnabled])
 
-
+  // bgmの振り分け
   useEffect(()=>{
     if(!audioEnabled){
   return
@@ -182,7 +184,7 @@ export const  Ui = () => {
           </button>
         )}
       </div>
-      {isStreamScreen() &&(
+      {isStreamScreen() && audioEnabled ?(
         <button className='fixed bottom-4 left-4 pointer-events-auto'
         onClick={toggleAudio}
         >
@@ -190,8 +192,23 @@ export const  Ui = () => {
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
           </svg>
 
+
+
         </button>
-      )}
+
+        
+      ):(
+        <button className='fixed bottom-4 left-4 pointer-events-auto'
+        onClick={toggleAudio}>
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+        </svg>
+
+        </button>
+
+      )
+      }
     </div>
   )
 }
